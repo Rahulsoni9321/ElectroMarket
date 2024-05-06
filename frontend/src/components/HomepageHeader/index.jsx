afdae75b82927ea2d210d9e5e7df64d0ce4af3da
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-05-07 04:52:30 +0530

    update files

--- frontend/src/components/HomepageHeader/index.jsx
+++ frontend/src/components/HomepageHeader/index.jsx
@@ -17,10 +17,17 @@ export default function HomepageHeader({
     <div {...props}>
       <header className="flex items-center justify-center self-stretch border-b border-solid border-gray-200 bg-white-A700 py-10 sm:py-5">
         <div className="container-xs flex items-center justify-between gap-5 md:flex-col md:p-5">
-          <Img src="images/img_header_logo.png" alt="headerlogo" className="h-[50px] w-[223px] object-contain" />
+          <Img
+            src="./public/Images/ElectroMarket.jpg"
+            alt="headerlogo"
+            className="h-[50px] w-[223px] object-contain"
+          />
           <ul className="flex flex-wrap items-center gap-10 pr-2.5">
             <li>
-              <a href="#" className="border-b-2 border-solid border-deep_orange-A700">
+              <a
+                href="#"
+                className="border-b-2 border-solid border-deep_orange-A700"
+              >
                 <Text size="2xl" as="p" className="p-[11px] tracking-[1.40px]">
                   {home}
                 </Text>
@@ -57,13 +64,25 @@ export default function HomepageHeader({
           </ul>
           <div className="flex w-[13%] justify-between gap-5 md:w-full">
             <a href="#">
-              <Img src="images/img_search.svg" alt="search" className="h-[32px] w-[32px]" />
+              <Img
+                src="images/img_search.svg"
+                alt="search"
+                className="h-[32px] w-[32px]"
+              />
             </a>
             <a href="#">
-              <Img src="images/img_bag.svg" alt="bag" className="h-[32px] w-[32px]" />
+              <Img
+                src="images/img_bag.svg"
+                alt="bag"
+                className="h-[32px] w-[32px]"
+              />
             </a>
             <a href="#">
-              <Img src="images/img_lock.svg" alt="lock" className="h-[32px] w-[32px]" />
+              <Img
+                src="images/img_lock.svg"
+                alt="lock"
+                className="h-[32px] w-[32px]"
+              />
             </a>
           </div>
         </div>
@@ -74,11 +93,25 @@ export default function HomepageHeader({
             <Text size="xl" as="p" className="capitalize !text-gray-700">
               {homeOne}
             </Text>
-            <Img src="images/img_arrow_right_gray_700.svg" alt="arrowright" className="h-[24px] w-[24px] sm:w-full" />
-            <Text size="xl" as="p" className="!font-medium capitalize !text-gray-700">
+            <Img
+              src="images/img_arrow_right_gray_700.svg"
+              alt="arrowright"
+              className="h-[24px] w-[24px] sm:w-full"
+            />
+            <Text
+              size="xl"
+              as="p"
+              className="!font-medium capitalize !text-gray-700"
+            >
               {shopOne}
             </Text>
-            {!!arrowright ? <Img src={arrowright} alt="arrowright" className="h-[24px] w-[24px] sm:w-full" /> : null}
+            {!!arrowright ? (
+              <Img
+                src={arrowright}
+                alt="arrowright"
+                className="h-[24px] w-[24px] sm:w-full"
+              />
+            ) : null}
             {!!shopTwo ? (
               <Text size="xl" as="p" className="!font-medium capitalize">
                 {shopTwo}
