29248d559b104fa6df0bbd905028051132141834
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-05-12 03:04:44 +0530

    add prop validation

--- frontend/src/components/HomepageHerosection/index.jsx
+++ frontend/src/components/HomepageHerosection/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Img, Text, Heading, Slider } from "./..";
+import PropTypes from "prop-types";
 
 export default function HomepageHerosection({
   titletext = (
@@ -17,12 +18,19 @@ export default function HomepageHerosection({
   const sliderRef = React.useRef(null);
 
   return (
-    <div {...props} className={`${props.className} flex flex-col items-center w-full mt-10 gap-10`}>
+    <div
+      {...props}
+      className={`${props.className} flex flex-col items-center w-full mt-10 gap-10`}
+    >
       <div className="container-xs flex w-full max-w-[1360px] rounded-[10px] bg-white-A700 md:p-5">
         <Slider
           autoPlay
           autoPlayInterval={2000}
-          responsive={{ 0: { items: 1 }, 550: { items: 1 }, 1050: { items: 1 } }}
+          responsive={{
+            0: { items: 1 },
+            550: { items: 1 },
+            1050: { items: 1 },
+          }}
           disableDotsControls
           activeIndex={sliderState}
           onSlideChanged={(e) => {
@@ -34,15 +42,29 @@ export default function HomepageHerosection({
               <div className="flex rounded-[10px] bg-white-A700">
                 <div className="flex h-[640px] w-full items-center rounded-[10px] bg-[url(/public/images/img_group_2.png)] bg-cover bg-no-repeat pb-[152px] pl-[63px] pr-14 pt-[181px] md:h-auto md:p-5">
                   <div className="flex w-[59%] flex-col items-start gap-9 md:w-full">
-                    <Heading size="7xl" as="h1" className="w-full leading-[114.99%] !text-white-A700">
+                    <Heading
+                      size="7xl"
+                      as="h1"
+                      className="w-full leading-[114.99%] !text-white-A700"
+                    >
                       {titletext}
                     </Heading>
-                    <Text size="xl" as="p" className="!font-medium !text-white-A700">
+                    <Text
+                      size="xl"
+                      as="p"
+                      className="!font-medium !text-white-A700"
+                    >
                       {descriptiontext}
                     </Text>
                     <Button
                       size="lg"
-                      rightIcon={<Img src="images/img_frame.svg" alt="frame" className="h-[24px] w-[24px]" />}
+                      rightIcon={
+                        <Img
+                          src="images/img_frame.svg"
+                          alt="frame"
+                          className="h-[24px] w-[24px]"
+                        />
+                      }
                       className="min-w-[167px] gap-2.5 rounded-md font-bold sm:px-5"
                     >
                       {button}
@@ -60,3 +82,9 @@ export default function HomepageHerosection({
     </div>
   );
 }
+HomepageHerosection.propTypes = {
+  titletext: PropTypes.string,
+  descriptiontext: PropTypes.string,
+  button: PropTypes.string,
+  className: PropTypes.string,
+};
