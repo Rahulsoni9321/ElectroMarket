66717102cbf38062d219c735ab4a50fc936a873d
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-05-17 02:51:54 +0530

    update

--- frontend/src/pages/CreateProduct/index.jsx
+++ frontend/src/pages/CreateProduct/index.jsx
@@ -1,8 +1,10 @@
 // components/AddProductForm.js
 
 import { useState } from "react";
+import Helmet from "react-helmet";
 import Chatbot from "../ChatBot";
-
+import Footer from "../../components/Footer";
+import HomepageHeader from "../../components/HomepageHeader";
 const CreateProduct = () => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -41,141 +43,154 @@ const CreateProduct = () => {
   };
 
   return (
-    <div className="max-w-md mx-auto mt-8">
-      <form
-        onSubmit={handleSubmit}
-        className="space-y-6"
-        encType="multipart/form-data"
-      >
-        <div>
-          <label
-            htmlFor="title"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Title
-          </label>
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          ></textarea>
-        </div>
-        <div>
-          <label
-            htmlFor="image"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Image
-          </label>
-          <input
-            type="file"
-            name="image"
-            id="image"
-            accept="image/*"
-            onChange={(e) => setImage(e.target.files[0])}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="price"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Price
-          </label>
-          <input
-            type="number"
-            name="price"
-            id="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="youtubeLink"
-            className="block text-sm font-medium text-gray-700"
-          >
-            YouTube Link
-          </label>
-          <input
-            type="url"
-            name="youtubeLink"
-            id="youtubeLink"
-            value={youtubeLink}
-            onChange={(e) => setYoutubeLink(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="adminName"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Admin Name
-          </label>
-          <input
-            type="text"
-            name="adminName"
-            id="adminName"
-            value={adminName}
-            onChange={(e) => setAdminName(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="adminEmail"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Admin Email
-          </label>
-          <input
-            type="email"
-            name="adminEmail"
-            id="adminEmail"
-            value={adminEmail}
-            onChange={(e) => setAdminEmail(e.target.value)}
-            required
-            className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Add Product
-          </button>
-        </div>
-      </form>
-      <Chatbot />
-    </div>
+    <>
+      <Helmet>
+        <title>ElectroMarket</title>
+        <meta name="description" content="" />
+      </Helmet>
+      <div className="flex w-full flex-col items-center gap-10 bg-white-A700 dark:bg-gray-800">
+        <HomepageHeader shopOne="Sign in" className="self-stretch" />
+        <section className="bg-gray-50 dark:bg-gray-300 self-stretch">
+          <div className="max-w-md mx-auto mt-8">
+            <form
+              onSubmit={handleSubmit}
+              className="space-y-6"
+              encType="multipart/form-data"
+            >
+              <div>
+                <label
+                  htmlFor="image"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Image
+                </label>
+                <input
+                  type="file"
+                  name="image"
+                  id="image"
+                  accept="image/*"
+                  onChange={(e) => setImage(e.target.files[0])}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="title"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Title
+                </label>
+                <input
+                  type="text"
+                  name="title"
+                  id="title"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="price"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Price
+                </label>
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="description"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="description"
+                  name="description"
+                  value={description}
+                  onChange={(e) => setDescription(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                ></textarea>
+              </div>
+
+              <div>
+                <label
+                  htmlFor="youtubeLink"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  YouTube Link
+                </label>
+                <input
+                  type="url"
+                  name="youtubeLink"
+                  id="youtubeLink"
+                  value={youtubeLink}
+                  onChange={(e) => setYoutubeLink(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="adminName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Admin Name
+                </label>
+                <input
+                  type="text"
+                  name="adminName"
+                  id="adminName"
+                  value={adminName}
+                  onChange={(e) => setAdminName(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <label
+                  htmlFor="adminEmail"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Admin Email
+                </label>
+                <input
+                  type="email"
+                  name="adminEmail"
+                  id="adminEmail"
+                  value={adminEmail}
+                  onChange={(e) => setAdminEmail(e.target.value)}
+                  required
+                  className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                />
+              </div>
+              <div>
+                <button
+                  type="submit"
+                  className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Add Product
+                </button>
+              </div>
+            </form>
+            <Chatbot />
+          </div>
+        </section>
+      </div>
+      <Footer className="self-stretch" />
+    </>
   );
 };
 
