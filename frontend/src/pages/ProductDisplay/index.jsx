bded1b2e130c91acf2d34fc1d410417671329cae
Sunil Kumar Saini <sunilkumarsaini6363@gmail.com>
2024-05-17 10:58:30 +0530

    update

--- frontend/src/pages/ProductDisplay/index.jsx
+++ frontend/src/pages/ProductDisplay/index.jsx
@@ -29,12 +29,11 @@ function ProductDisplay() {
 
               <div className="mt-6 sm:mt-8 lg:mt-0">
                 <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
-                  Apple iMac 24 All-In-One Computer, Apple M1, 8GB RAM, 256GB
-                  SSD, Mac OS, Pink
+                  Product name
                 </h1>
                 <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
                   <p className="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
-                    $1,249.99
+                    price
                   </p>
 
                   <div className="flex items-center gap-2 mt-2 sm:mt-0">
@@ -102,7 +101,7 @@ function ProductDisplay() {
                       href="#"
                       className="text-sm font-medium leading-none text-gray-900 underline hover:no-underline dark:text-white"
                     >
-                      345 Reviews
+                      Reviews
                     </a>
                   </div>
                 </div>
@@ -164,17 +163,7 @@ function ProductDisplay() {
                 <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
 
                 <p className="mb-6 text-gray-500 dark:text-gray-400">
-                  Studio quality three mic array for crystal clear calls and
-                  voice recordings. Six-speaker sound system for a remarkably
-                  robust and high-quality audio experience. Up to 256GB of
-                  ultrafast SSD storage.
-                </p>
-
-                <p className="text-gray-500 dark:text-gray-400">
-                  Two Thunderbolt USB 4 ports and up to two USB 3 ports.
-                  Ultrafast Wi-Fi 6 and Bluetooth 5.0 wireless. Color matched
-                  Magic Mouse with Magic Keyboard or Magic Keyboard with Touch
-                  ID.
+                  description
                 </p>
               </div>
             </div>
